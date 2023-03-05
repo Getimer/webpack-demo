8a6f03dd3ac60f3f57e6f3c670892098f5f3ac53
nmer <2309382194@qq.com>
2023-03-05 21:30:23 +0800

    add scss

---
+++ webpack.config.base.js
@@ -0,0 +1,31 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "[name].[contenthash].js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "csg app",
+      template: "src/assets/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require("dart-sass"),
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,14 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
+const base = require("./webpack.config.base.js");
 module.exports = {
+  ...base,
   mode: "development",
-  entry: "./src/index.js",
-  output: {
-    filename: "[name].[contenthash].js",
-  },
   devtool: "inline-source-map",
   devServer: {
     contentBase: "./dist",
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "csg app",
-      template: "src/assets/index.html",
-    }),
-  ],
   module: {
     rules: [
+      ...base.module.rules,
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
